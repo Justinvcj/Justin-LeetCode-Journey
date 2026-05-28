efaeaab7afa65fc7b45850dab83ba60e8eca9925
Justin Varghese <164189693+Justinvcj@users.noreply.github.com>
2026-05-28 19:12:40 +0530

    Time: 5 ms (68.46%) | Memory: 43.7 MB (27.68%) - LeetSync

--- 274-h-index/h-index.java
+++ 274-h-index/h-index.java
@@ -1,32 +1,22 @@
 class Solution 
 {
 
-    public void reverse(int [] arr,int left,int right)
-    {
-        while(left<right){
-            int temp=arr[left];
-            arr[left]=arr[right];
-            arr[right]=temp;
-            left++;
-            right--;
-        }
-    }
     public int hIndex(int[] arr) 
     {
         int n=arr.length;
         Arrays.sort(arr);
-       reverse(arr,0,n-1);
+      
         int paper=0;
-        int count=0;
+        
         for(int i=0;i<n;i++)
         {
-            paper=i+1;
+            paper=n-i;
             if(arr[i]>=paper){
-                count=paper;
+                return paper;
             }
         }
         
-     return count;      
+     return 0;      
     }
     
 }
