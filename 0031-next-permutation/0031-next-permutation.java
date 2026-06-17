5e05a6b7e34c9fa40f387030972c2596648bbc3d
Justin Varghese <164189693+Justinvcj@users.noreply.github.com>
2026-06-17 15:59:58 +0530

    Time: 0 ms (100%), Space: 44.7 MB (31.35%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.java
@@ -0,0 +1,44 @@
+class Solution {
+
+    public void nextPermutation(int[] nums) {
+
+        int pivot = -1;
+
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                pivot = i;
+                break;
+            }
+        }
+
+        if (pivot != -1) {
+
+            for (int i = nums.length - 1; i > pivot; i--) {
+
+                if (nums[i] > nums[pivot]) {
+
+                    int temp = nums[i];
+                    nums[i] = nums[pivot];
+                    nums[pivot] = temp;
+
+                    break;
+                }
+            }
+        }
+
+        reverse(nums, pivot + 1, nums.length - 1);
+    }
+
+    public void reverse(int[] arr, int left, int right) {
+
+        while (left < right) {
+
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+}
