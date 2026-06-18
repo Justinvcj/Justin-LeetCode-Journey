f0a7534f5bd59ffd5a9b80c5c6b104324fee02d8
Justin Varghese <164189693+Justinvcj@users.noreply.github.com>
2026-06-18 13:42:39 +0530

    Time: 2 ms (62.87%), Space: 43.4 MB (53.14%) - LeetHub

--- 1796-second-largest-digit-in-a-string/1796-second-largest-digit-in-a-string.java
+++ 1796-second-largest-digit-in-a-string/1796-second-largest-digit-in-a-string.java
@@ -1,26 +1,21 @@
 class Solution {
     public int secondHighest(String s) {
-    List<Integer>list=new ArrayList<>();
-    
-   
-    for(int i=0;i<s.length();i++){
-        if(Character.isDigit(s.charAt(i))){
-            if(!list.contains(s.charAt(i)-'0')){
-                list.add(s.charAt(i)-'0');
-            }
-            
-        }
-    }
-    int [] arr=new int[list.size()];
-    for(int i=0;i<arr.length;i++){
-        arr[i]=list.get(i);
-    }
-    Arrays.sort(arr);
-    if(arr.length<2){
-        return -1;
-    }
-    return arr[arr.length-2];
-    
+    int first=-1;
+    int second=-1;
 
+    for(int i=0;i<s.length();i++){
+    char c=s.charAt(i);
+    if(Character.isDigit(c)){
+        int digit=c-'0';
+
+        if(digit>first){
+            second=first;
+            first=digit;
+        }else if(digit>second && digit!= first){
+            second=digit;
+        }
+    }   
+    } 
+    return second;   
     }
 }
