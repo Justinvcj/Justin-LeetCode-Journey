9f9c1d7617b3934f6075e366f63ef9dc335eaab6
Justin Varghese <164189693+Justinvcj@users.noreply.github.com>
2026-06-22 14:35:09 +0530

    Time: 6 ms (26.98%), Space: 46.1 MB (93.07%) - LeetHub

---
+++ 1513-number-of-substrings-with-only-1s/1513-number-of-substrings-with-only-1s.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numSub(String s) {
+        long ans=0;
+        long count=0;
+        long mod=1000000007;
+
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='1'){
+                count++;
+            }else{
+                count=0;
+            }
+            ans=(ans+count)%mod;
+        }
+        return (int)ans;
+    }
+}
