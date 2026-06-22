bc469cf254d545f3c1dfcb9a8e7dd041fc648be4
Justin Varghese <164189693+Justinvcj@users.noreply.github.com>
2026-06-22 14:08:20 +0530

    Time: 2 ms (47.49%), Space: 46.9 MB (30.68%) - LeetHub

---
+++ 0674-longest-continuous-increasing-subsequence/0674-longest-continuous-increasing-subsequence.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findLengthOfLCIS(int[] arr) {
+    int ans=1;
+    int count=1;
+
+    for(int i=0;i<arr.length-1;i++){
+        if(arr[i]<arr[i+1]){
+            count++;
+        }else{
+            count=1;
+        }
+        ans=Math.max(ans,count);
+    }
+    return ans;    
+    }
+}
