c2d38c81616b5349fe2bc5c601f6294143a78b37
Justin Varghese <164189693+Justinvcj@users.noreply.github.com>
2026-06-27 11:53:17 +0530

    Time: 1 ms (99.96%), Space: 46.2 MB (89.57%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+    int [] freq=new int[26];
+
+    for(char ch:magazine.toCharArray()){
+        freq[ch-'a']++;
+    }
+    for(int i=0;i<ransomNote.length();i++){
+        int idx=ransomNote.charAt(i)-'a';
+
+        if(freq[idx]==0){
+            return false;
+        }
+        freq[idx]--;
+    }  
+    return true;  
+    }
+}
