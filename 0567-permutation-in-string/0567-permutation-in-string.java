15df7e85d493fa20deeaa9d53eb87cc4d7f43955
Justin Varghese <164189693+Justinvcj@users.noreply.github.com>
2026-07-01 16:13:34 +0530

    Time: 5 ms (97.57%), Space: 44.2 MB (39%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+    int [] freq =new int [26];
+    for(char c:s1.toCharArray()){
+        freq[c-'a']++;
+    }
+    int left=0;
+    for(int right=0;right<s2.length();right++)
+    {
+        int rightchar=s2.charAt(right)-'a';
+        freq[rightchar]--;
+
+        while(freq[rightchar]<0){
+            int leftchar=s2.charAt(left)-'a';
+            freq[leftchar]++;
+            left++;
+        }
+        if(right-left+1==s1.length()){
+            return true;
+        }
+    }
+    return false;
+    }
+}
